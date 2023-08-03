026f38d04e7a3ae30ae8f12b5bd6248eea975839
dozen3 <140199159+dozen3@users.noreply.github.com>
2023-08-03 18:11:09 +0000

    more config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,19 +2,23 @@
 module.exports = {
   content: ["index.html"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'Handjet': ['Handjet', 'sans-serif']
+      },
     fontSize: {
       sm: '0.8rem',
       base: '1rem',
       xl: '1.25rem',
       '2xl': '1.563rem',
-      '3xl': ['1.875rem', {
+      '3xl': ['3rem', {
         lineHeight: '2.25rem',
-        fontWeight: '700',
+        fontWeight: '700'
       }],
       '4xl': '2.441rem',
       '5xl': '3.052rem',
     }
+  }
   },
   plugins: [],
 }
