c49d2c88ec2096c80906869b1288c40c8175ca07
dozen3 <140199159+dozen3@users.noreply.github.com>
2023-08-03 11:19:46 -0600

    New config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,18 @@ module.exports = {
   content: ["index.html"],
   theme: {
     extend: {},
+    fontSize: {
+      sm: '0.8rem',
+      base: '1rem',
+      xl: '1.25rem',
+      '2xl': '1.563rem',
+      '3xl': ['1.875rem', {
+        lineHeight: '2.25rem',
+        fontWeight: '700',
+      }],
+      '4xl': '2.441rem',
+      '5xl': '3.052rem',
+    }
   },
   plugins: [],
 }
